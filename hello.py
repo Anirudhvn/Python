f8ec09f2a77badd7670dcd92fa83401e4de65935
Anirudhvn <anirudhvarasala@gmail.com>
2024-04-18 11:58:38 +0530

    18-04-2024

---
+++ hello.py
@@ -0,0 +1,20 @@
+# print(6)
+# #this is hello world program
+# print("hello world")
+
+# a,b,c,=1,20.2,"hello world"
+# print(a)
+# print(a,b)
+
+# a=10
+# b=20.3
+# print (a+b)
+
+a="hello"
+b="world"
+# # print(a+b)
+# print(a+" "+b)
+# print(type(a))
+# print(type(b))
+# print(len(a))
+print(a+str(4)+b)
